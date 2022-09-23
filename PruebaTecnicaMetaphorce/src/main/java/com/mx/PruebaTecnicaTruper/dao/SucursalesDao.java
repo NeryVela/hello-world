f217e5b4593b153670350c192ce888a36056bca0
Nery Vela <leoingenieria@gmail.com>
2022-09-23 08:31:47 -0500

    DELATE:eliminado

--- PruebaTecnicaMetaphorce/src/main/java/com/mx/PruebaTecnicaTruper/dao/SucursalesDao.java
+++
@@ -1,9 +0,0 @@
-package com.mx.PruebaTecnicaTruper.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mx.PruebaTecnicaTruper.dominio.*;
-
-public interface SucursalesDao extends JpaRepository<Sucursales,Integer> {
-
-}
